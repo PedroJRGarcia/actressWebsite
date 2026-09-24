a6e2f473881b61f6b18b759218a2af1d6525317f
Pedro <garciapj85@gmail.com>
2026-09-24 03:17:06 +0200

    start page changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,31 +3,40 @@ import { profile } from '../data/profile'
 import type { SectionProps } from '../i18n'
 
 export const Home: React.FC<SectionProps> = ({ t }) => (
-  <section id='home' className='home'>
-    <div className='prints'>
-      {profile.homePhotos.map((photo, index) => (
-        <figure key={`${index}-${photo}`} className='print'>
-          <img src={photo} alt={`${profile.name} ${index + 1}`} />
-        </figure>
-      ))}
-    </div>
+  <>
+    {/* Arrival: one big photo with the name and the showreel button */}
+    <section id='home' className='hero'>
+      <img src={profile.heroPhoto} alt={profile.name} />
+      <div className='hero-content'>
+        <h1>{profile.name}</h1>
+        <p className='hero-role'>{t.role}</p>
+        <a className='button' href='#showreels'>
+          {t.watchReel}
+        </a>
+      </div>
+    </section>
 
-    <div className='home-info'>
-      <h1>{profile.name}</h1>
-      <p className='home-role'>{t.role}</p>
-      <p className='muted'>{t.basedIn}</p>
-      <a className='button' href='#showreels'>
-        {t.watchReel}
-      </a>
-    </div>
-
-    <dl className='stats'>
-      {t.stats.map(stat => (
-        <div key={stat.label}>
-          <dt>{stat.label}</dt>
-          <dd>{stat.value}</dd>
-        </div>
-      ))}
-    </dl>
-  </section>
+    {/* A little about Elina, then her details */}
+    <section className='about'>
+      <div className='prints'>
+        {profile.homePhotos.map((photo, index) => (
+          <figure key={`${index}-${photo}`} className='print'>
+            <img src={photo} alt={`${profile.name} ${index + 1}`} loading='lazy' />
+          </figure>
+        ))}
+      </div>
+      <div>
+        <h2>{t.about.title}</h2>
+        <p className='about-text'>{t.about.text}</p>
+      </div>
+      <dl className='stats'>
+        {t.stats.map(stat => (
+          <div key={stat.label}>
+            <dt>{stat.label}</dt>
+            <dd>{stat.value}</dd>
+          </div>
+        ))}
+      </dl>
+    </section>
+  </>
 )
